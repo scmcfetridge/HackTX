159da64bfb4d4960a75edec4bf1bff1004af03f9
Paige Hinkle <paigehinkle@utexas.edu>
2013-11-15 22:06:21 -0600

    adding AllGames

--- src/com/hacktx/picwars/Login.java
+++ src/com/hacktx/picwars/Login.java
@@ -24,7 +24,7 @@ public class Login extends Activity {
 	
 	private void parseSetup(){
 		Parse.initialize(this, "6xMmCvXeB12R6L2p9DvoYbUnHHMITaPuz0PM1Jrx", "m2Qka0510lN2c6MwFy4m1CRGAOFXb2XYRBJXzlJU");
-		
+
 		ParseAnalytics.trackAppOpened(getIntent());
 	}
 
